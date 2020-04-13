e21286b0a1e1f972189222c5720b805f9406a3ed
Patrick McDowell <kuhlthing@gmail.com>
2020-04-12 23:30:53 -0400

    adding links to portfolio

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -15,7 +15,9 @@ import Navbar from './Navbar';
 import project1 from "../images/getInfoScrn2.png";
 import project2 from "../images/spotifeelsScrn.png";
 import project3 from "../images/budgetScrn.png";
-import project4 from "../images/PassGen.png"
+import project4 from "../images/PassGen.png";
+import project5 from "../images/Quiz.png";
+import project6 from "../images/Screenshot.png"
 
 const UseStyles = makeStyles({
     mainContainer: {
@@ -52,16 +54,16 @@ const portfolio = () => {
                                     Simple way to search for a news topic and compare headlines between two news sources.
                                 </Typography>
                             </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Share
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
                                 </Button>
-                                <Button size="small" color="primary">
-                                    Live Demo
-                                </Button>
-                            </CardActions>
-                        
+                            <Button size="small" color="primary">
+                                <a href="#" target="_blank" rel="noopener noreferrer"> Live Demo </a>
+                            </Button>
+                        </CardActions>
+
                     </Card>
                 </Grid>
                 {/* Project 2 */}
@@ -79,7 +81,7 @@ const portfolio = () => {
                                     Project 2
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
-                                    An app that collects subjective data on songs and matches them to your chosen mood.
+                                    An app that collects subjective data on songs from users and matches them to your chosen mood.
                                 </Typography>
                             </CardContent>
                         </CardActionArea>
@@ -88,8 +90,8 @@ const portfolio = () => {
                                 Share
                                 </Button>
                             <Button size="small" color="primary">
-                                Live Demo
-                                </Button>
+                                <a href="#" target="_blank" rel="noopener noreferrer"> Live Demo </a>
+                            </Button>
                         </CardActions>
                     </Card>
                 </Grid>
@@ -105,21 +107,21 @@ const portfolio = () => {
                             />
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
-                                    Project 3
+                                    Budget Tracker
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     A simple barebones app to balance your personal budget.
                                 </Typography>
-                            </CardContent>                        
-                            </CardActionArea>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Share
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
                                 </Button>
-                                <Button size="small" color="primary">
-                                    Live Demo
-                                </Button>
-                            </CardActions>
+                            <Button size="small" color="primary">
+                                <a href="https://secure-oasis-13528.herokuapp.com/" target="_blank" rel="noopener noreferrer">Live Demo </a>
+                            </Button>
+                        </CardActions>
                     </Card>
                 </Grid>
                 {/* Project 4 */}
@@ -134,7 +136,7 @@ const portfolio = () => {
                             />
                             <CardContent>
                                 <Typography gutterBottom variant="h5">
-                                    Project 4
+                                    Password Generator
                                 </Typography>
                                 <Typography variant="body2" color="textSecondary" component="p">
                                     This app generates a password made up of random characters based on parameters chosen by the user such as length and character type.
@@ -146,13 +148,72 @@ const portfolio = () => {
                                 Share
                                 </Button>
                             <Button size="small" color="primary">
-                                Live Demo
-                                </Button>
+                                <a href="https://kuhlthing.github.io/passwordgen/" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </Button>
                         </CardActions>
                     </Card>
                 </Grid>
+                {/* Project 5 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project 5"
+                                height="200"
+                                image={project5}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Trivia Quiz
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    A quiz application featuring random trivia questions.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                                </Button>
+                            <Button size="small" color="primary">
+                                <a href="https://kuhlthing.github.io/JavaScriptQuiz/" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </Button>
+                        </CardActions>
+
+                    </Card>
+                </Grid>
+                {/* Project 6 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                                component="img"
+                                alt="Project 6"
+                                height="200"
+                                image={project6}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Employee Tracker
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary" component="p">
+                                    Simple offline employee organization database that runs on a localhost and can be updated via terminal.
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button size="small" color="primary">
+                                Share
+                                </Button>
+                            <Button size="small" color="primary">
+                                <a href="https://github.com/KuhlThing/EmployeeTracker" target="_blank" rel="noopener noreferrer">Live Demo</a>
+                            </Button>
+                        </CardActions>
+
+                    </Card>
+                </Grid>
             </Grid>
-            Hello from Portfolio
         </Box>
     )
 }
